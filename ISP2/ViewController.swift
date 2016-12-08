035c57870c1c825c8c921169e6c7fbf234a20062
Student <student@macbook-pro-426-mcilroy.local>
2016-12-08 10:44:41 -0500

    Initial Commit

---
+++ ISP2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ISP2
+//
+//  Created by Student on 2016-12-08.
+//  Copyright © 2016 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
