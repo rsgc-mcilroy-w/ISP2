e4cfc3182ee9b96865760953abb82774a33bdac8
Student <student@macbook-pro-426-mcilroy.local>
2016-12-16 10:50:40 -0500

    midway through auto layout

--- ISP2/ViewController.swift
+++ ISP2/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  
+    @IBOutlet weak var number: UITextField!
+    @IBOutlet weak var string: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func factorize(_ sender: Any) {
+        func factor(number: Int) -> String {
+            var string = ""
+            for i in 1...number {
+                if number % i == 0 {
+                    if i == 1 {
+                        string += "\(i)"
+                    } else {
+                        string += "and \(i)"
+                    }
+                }
+            }
+            return string
+        }
+    }
 
 }
+
+
 
