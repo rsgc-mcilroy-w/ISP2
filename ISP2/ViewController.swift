f270829c879f7b2b6ddc6f09a3d72463d2810c1a
Student <student@macbook-pro-426-mcilroy.local>
2017-01-03 16:22:38 -0500

    Got outlets and action set up. Struggling with how to let the factoring function accept the first textfield as the "Int" component to the function.

--- ISP2/ViewController.swift
+++ ISP2/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  
-    @IBOutlet weak var number: UITextField!
-    @IBOutlet weak var string: UITextField!
+    //outlets
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textFieldResult: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,21 +26,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
     @IBAction func factorize(_ sender: Any) {
-        func factor(number: Int) -> String {
-            var string = ""
-            for i in 1...number {
-                if number % i == 0 {
-                    if i == 1 {
-                        string += "\(i)"
-                    } else {
-                        string += "and \(i)"
+       guard let textFieldAsString = textField.text else { return
+     }
+    
+        
+        func factor(number: Int) -> String {    //
+            var result = ""                     //
+            for i in 1...number {               //
+                if number % i == 0 {            //
+                    if i == 1 {                 //
+                        result += "\(i)"        //
+                    } else {                    //
+                        result += "and \(i)"    //
                     }
                 }
             }
-            return string
+            return result                       //
         }
+          textFieldResult.text = "who knows"
     }
+  
 
 }
 
